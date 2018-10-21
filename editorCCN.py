fc3358cb4d41771c501b101fb41d79c663115e9f
Josiah Wilmoth <josiahwilmoth@gmail.com>
2018-10-20 22:15:31 -0400

    Update editorCCN.py

--- editorCCN.py
+++ editorCCN.py
@@ -6,11 +6,10 @@ import pyperclip, re, sys
 #To Do:
 #Figure out how to capitalize the first letter of a paragraph
 
-
-
-
 searchAndReplace = {
 #Hardcoded Search and Replace Dictionary
+	#
+	#Industry vocabulary
 	"Bitcoin Cash" : "bitcoin cash",
 	"Ethereum Classic" : "ethereum classic",
 	"Bitcoin" : "bitcoin",
@@ -19,6 +18,7 @@ searchAndReplace = {
 	"Litecoin" : "litecoin",
 	"Ripple price" : "ripple price",
 	"Dash" : "dash",
+	"Dogecoin" : "dogecoin",
 	"Monero" : "monero",
 	"Stellar price" : "stellar price",
 	"Tezos" : "tezos",
@@ -27,7 +27,16 @@ searchAndReplace = {
 	"Cardano" : "cardano",
 	"VeChain" : "vechain",
 	"Zcash" : "zcash",
-	"Blockchain" : "blockchain"
+	"Blockchain" : "blockchain",
+	"Petro" : "petro",
+	"DApp" : "dApp",
+	"dapp" : "dApp",
+	"Initial Coin Offering" : "initial coin offering",
+	"FinTech" : "fintech",
+	"Fintech" : "fintech",
+	#
+	#Misc. Edits
+	"in order to" : "to"
 	}
 
 #Functions
@@ -36,11 +45,14 @@ def search_and_replace(text, edits, attribution):
 	#Edits the article
 	for item in edits.keys():
 		text = re.sub(item, edits[item], text)
-
 	text = re.sub("<div>", "", text)
+	text = re.sub("<div class=\"hardwall\">", "", text)
 	text = re.sub("</div>", "", text)
+	text = re.sub("<span>", "", text)
 	text = re.sub("</span>", "", text)
 	text = re.sub("<span style=\"font-weight: 400;\">", "", text)
+	text = re.sub("<span lang=\"EN-US\">", "", text)
+	text = re.sub("<span style=\"color: #000000;\">", "", text)
 	text = re.sub("<b>", "<strong>", text)
 	text = re.sub("</b>", "</strong>", text)
 	text = re.sub("<i>", "<em>", text)
@@ -50,11 +62,21 @@ def search_and_replace(text, edits, attribution):
 	text = re.sub("</h2></strong>", "</h2>", text)
 	text = re.sub(r'(\. )([a-z])', capitalize, text)
 	text = re.sub(r'([A-Z]\.[A-Z]\. )([A-Z])', fix_acronyms, text)
-	text = re.sub(r'(<h*>)([a-z])', fix_headings, text)
+	text = re.sub(r'(<h*>)([a-z])', capitalize, text)
 	text = re.sub("%", " percent", text)
+	text = re.sub(r'(\"\.)', '."', text)
+	text = re.sub(r'(\”\.)', '."', text)
+	text = re.sub(r'(\"\,)', ',"', text)
+	text = re.sub(r'(\”\,)', ',"', text)
+	text = re.sub("–", "--", text)
+	text = re.sub(r'(\s\s)', " ", text)
 
-	if attribution == 1:
+	if attribution == 0:
+		text = text +('\n\n<em>Featured Image from Shutterstock</em>')
+	elif attribution == 1:
 		text = text + ('\n\n<em>Featured Image from Shutterstock. Charts from <a href="https://www.tradingview.com/ideas/cryptocurrency/" target="blank" rel="nofollow">TradingView</a>.</em>')
+	elif attribution == 2:
+		text = text + ('\n\n<em>Disclaimer: The views expressed in the article are solely those of the author and do not represent those of, nor should they be attributed to, CCN.</em>\n\n<em>Images from Shutterstock</em>')
 	return text
 
 def capitalize(m):
@@ -66,18 +88,14 @@ def fix_acronyms(m):
 	# Ensures that the first letter after "U.S." and "U.K." is lowercase.
 	return m.groups()[0] + m.groups()[1].lower()
 
-def fix_headings(m):
-	#Ensures the first word of headings are capitalized
-	return m.groups()[0] + m.groups()[1].upper()
-
 #Variables
 
 imageAttribution = 0
 
-print(r'Instructions: Copy the raw text of the article to your clipboard.')
+print('\nInstructions: Copy the raw text of the article to your clipboard.')
 print(r'Press ENTER to run the program without an image attribution.')
-print(r'''Enter 'C' to run the program and add an 'Images from Shutterstock and Charts from TradingView' attribution.
-''')
+print(r'''Enter 'O' to run the program for an opinion piece.''')
+print(r'''Enter 'P' to run the program for price analysis articles.''')
 
 while True:
 	while True:
@@ -85,15 +103,19 @@ while True:
 		if answer == '':
 			imageAttribution = 0
 			break
-		elif answer == 'c' or answer == 'C':
+		elif answer == 'p' or answer == 'P':
 			imageAttribution = 1
+			break
+		elif answer == 'o' or answer == 'O':
+			imageAttribution = 2
 			break
 		elif answer == 'x' or answer == 'X':
 			sys.exit()
 		else:
 			print('\nInvalid entry.')
 			print(r'Press ENTER to run again without an attribution.')
-			print(r'''Enter 'C' to run the program and add an 'Images from Shutterstock and Charts from TradingView' attribution.''')
+			print(r'''Enter 'O' to run the program for an opinion piece.''')
+			print(r'''Enter 'P' to run the program for price analysis articles.''')
 			print(r'''Enter 'X' to Quit: ''')
 
 	draft = str(pyperclip.paste())
@@ -101,5 +123,6 @@ while True:
 
 	print(r'Edited text copied to clipboard.')
 	print(r'Press ENTER to run again without an attribution.')
-	print(r'''Enter 'C' to run the program and add an 'Images from Shutterstock and Charts from TradingView' attribution.''')
+	print(r'''Enter 'O' to run the program for an opinion piece.''')
+	print(r'''Enter 'P' to run the program for price analysis articles.''')
 	print(r'''Enter 'X' to Quit: ''')
